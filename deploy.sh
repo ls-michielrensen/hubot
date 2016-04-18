43282d4c0f9d641b2e1e14d62855fff4ce2fcb80
Michiel Rensen <info@michielrensen.nl>
2016-04-19 00:01:52 +0200

    Even simpler EB deployments (hopefully)

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ EB_BUCKET=hello-bucket
 DOCKERRUN_FILE=Dockerrun.aws.json
 sed -i'' -e "s;%BUILD_NUM%;$VERSION;g" ./Dockerrun.aws.json
 aws elasticbeanstalk create-application-version --application-name hubot \
-  --version-label $VERSION --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+  --version-label $VERSION
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name ls-hubot \
