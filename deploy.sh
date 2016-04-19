b5371c1e84621dde7224b28d05c3bbf55fd717c9
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2016-04-19 14:11:46 +0200

    Automatic deployments using S3 bucket

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,15 @@ VERSION=$1
 PROFILE=$2
 
 # Create new Elastic Beanstalk version
-sed -i'' "s;%BUILD_NUM%;$VERSION;g" ./Dockerrun.aws.json
+EB_BUCKET=cml-ls-hubot
+DOCKERRUN_FILE=$VERSION-Dockerrun.aws.json
+
+sed "s/<TAG>/$VERSION/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE $( if [ -n "${PROFILE}" ]; then echo "--profile $PROFILE"; fi )
+
 aws elasticbeanstalk create-application-version --application-name ls-hubot \
-  --version-label $VERSION --profile $PROFILE
+  --version-label $VERSION --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE $( if [ -n "${PROFILE}" ]; then echo "--profile $PROFILE"; fi )
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name ls-hubot-env \
-    --version-label $VERSION --profile $PROFILE
+    --version-label $VERSION $( if [ -n "${PROFILE}" ]; then echo "--profile $PROFILE"; fi )
